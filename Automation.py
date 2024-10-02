f617c6f375b6847114c0e0a33f36a471e9dda63b
Sivasankara Eswari S M <kala.sankari199@gmail.com>
2024-10-02 15:45:14 +0530

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -4,7 +4,7 @@ def workauto():
     Chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
     URLS = ("stackoverflow.com",
             "google.com",
-            "google.com",
+            "yahoo.com",
             "github.com/Eswari123-cloud")
     for url in URLS:
         wb.get(Chrome_path).open(url)
