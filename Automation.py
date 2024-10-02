c7613e394b09b00eb169e991adce53c0974a672c
Sivasankara Eswari S M <kala.sankari199@gmail.com>
2024-10-02 15:44:52 +0530

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -1,15 +1,13 @@
 import webbrowser as wb
 
 def workauto():
-    Chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
-    URLS = ("google.com",
-            "yahoo.com",
-            "github.com/Eswari123-cloud",
-            "youtube.com",
-            "gmail.com")
+    Chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    URLS = ("stackoverflow.com",
+            "google.com",
+            "google.com",
+            "github.com/Eswari123-cloud")
     for url in URLS:
         wb.get(Chrome_path).open(url)
-
 
     workauto()    
 
